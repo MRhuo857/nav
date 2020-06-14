9a26325d6891d7ba0d5f368cdfbefc94bd8365db
huomeng <455801665@qq.com>
2020-06-14 22:32:37 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,68 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "A", url: "http://www.daqianduan.com/nav" },
+  { logo: "B", url: "https://www.bilibili.com/" },
+];
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+    <div class="site">
+      <div class="logo">${node.logo}</div>
+      <div class="link">${simplifyUrl(node.url)}</div>
+      <div class="close">
+        <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-close"></use>
+        </svg>
+      </div>
+    </div>
+  </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      console.log("这里");
+      e.stopPropagation();
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+render();
+$(".addButton").on("click", () => {
+  let url = window.prompt("请输入您要添加的网址");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+
+  render();
+});
+
+//页面关闭前触发
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string); //设置全局变量
+};
+$(document).on("keypress", (e) => {
+  // const key = e.key;
+  const { key } = e;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLocaleLowerCase() === key) {
+      window.open(hashMap[i].url);
+    }
+  }
+});
